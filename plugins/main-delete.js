fd6fee98c40b98a2008b404a63dadb5a0db61a8b
RielTod01 <107606413+RielTod01@users.noreply.github.com>
2022-06-17 14:20:50 +0800

    Update main-delete.js

--- plugins/main-delete.js
+++ plugins/main-delete.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   if (!m.quoted) throw false
  let { chat, fromMe, id, isBaileys } = m.quoted
- if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+ if (!isBaileys) throw '𝐩𝐞𝐬𝐚𝐧 𝐭𝐞𝐫𝐬𝐞𝐛𝐮𝐭 𝐛𝐮𝐤𝐚𝐧 𝐝𝐢𝐤𝐢𝐫𝐢𝐦 𝐛𝐨𝐭 𝐦𝐞𝐦𝐞𝐤!'
  conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
 }
 handler.help = ['delete']
